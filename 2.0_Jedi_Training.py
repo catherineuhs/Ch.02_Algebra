f4b9f99c8db92e68682483a85edd177240b72c18
catherineuhs <cliffordcat@urbandaleschools.com>
2019-09-10 13:56:24 -0500

    turtle

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,23 +1,23 @@
 '''
-Sign your name:________________
+Sign your name:Catherine Clifford
 
 1.) How do you enter a single line comment in a program?
-
+Single-line comments are created simply by beginning a line with the hash (#) character, and they are automatically terminated by the end of line.
 
 
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                =2
+b**a                =25
+b%a                 = 1
+a+B                 = 7
+type(42)            =<class 'int'>
+type(42.0)          =<class 'float'>
+type("C3PO")        =<class 'str'>
+type(True)          =<class 'bool'>
 
 
 
@@ -30,8 +30,8 @@ a/=2
 a+=12
 a-=7
 
-a             =
-type(a)       =
+a             =15.0
+type(a)       =<class 'float'>
 
 
 
